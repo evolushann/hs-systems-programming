1ea1420ad17a6ab838becc1b65b926a88d3e06b1
Shannon B <shanbering@gmail.com>
2018-11-30 11:30:46 +0100

    :bug: fixed something

--- virtual-memory/shared.c
+++ virtual-memory/shared.c
@@ -19,15 +19,13 @@ int main() {
 
     pid_t pid = fork();
 
-    memcpy(mem, parent_message, sizeof(parent_message));
-
     while(1) {
         char* message = (char*) mem;
-	    if (pid == 0 && strcmp(mem, parent_message) == 0) {
+	    if (pid == 0 && strcmp(message, parent_message) == 0) {
     	    printf("Child read: %s\n", message);
             memcpy(mem, child_message, sizeof(child_message));
-	    } else if (strcmp(mem, child_message) == 0) {
-        	printf("Parent read: %s\n", (char*) mem);
+	    } else if (strcmp(message, parent_message) != 0) {
+        	printf("Parent read: %s\n", message);
             memcpy(mem, parent_message, sizeof(parent_message));
  	    }
     }
